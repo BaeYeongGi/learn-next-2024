2a77177bec5340972b24c65a3c92cf3565f3292c
BaeYeongGi <byg5913@gmail.com>
2024-07-23 19:47:18 +0900

    add: learn link component

---
+++ pages/home.jsx
@@ -0,0 +1,11 @@
+// '/' 에 해당되는 페이지 컴포넌트
+
+function HomePage() {
+	return (
+		<>
+			<div>Hello Next</div>
+		</>
+	);
+}
+
+export default HomePage;
---
+++ study.ts
@@ -0,0 +1,19 @@
+/*
+1) dependencies && devDependencies
+Application 로직에 직접적으로
+관여하는 = dependencies 
+관여하지 않는 = devDependencies
+
+2) Intellisense & Built in component
+Ctrl + Space 
+VScode에서 제공하는 미리보기 기능 & next 에서 내장된 컴포넌트 호출
+
+3) eslintrc.json
+코드 문법을 검사해주는 설정 파일
+
+4) Link
+Link 컴포넌트가 위치한 UI 영역이 뷰포트에 잡히면
+넥스트 내부적으로 해당 페이지 데이터를 미리 요청해둠(prefetching)
+미리 다음 페이지 데이터가 요청되어 있다면, 클릭했을 때 즉시 페이지 로딩이
+일어남
+*/
